bb1fab8c4d8b66dfcdf39557fd56068057d3edb4
shengxingyue <shengxingyue@souche.com>
2018-04-26 18:41:22 +0800

    feat : leetcode 001 TwoSum

---
+++ src/main/java/org/sheng/as/leetcode/TowSum_001.java
@@ -0,0 +1,19 @@
+package org.sheng.as.leetcode;
+
+/**
+ * @author shengxingyue on 2018/4/26 18:28
+ */
+public class TowSum_001 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
